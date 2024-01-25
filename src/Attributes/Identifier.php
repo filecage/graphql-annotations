8542200775c310ae9a56a4446119a3b49e92b4d5
David <filecage@users.noreply.github.com>
2024-01-25 13:44:40 +0100

    adds `Identifier` attribute

---
+++ src/Attributes/Identifier.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Filecage\GraphQL\Annotations\Attributes;
+
+#[\Attribute]
+/**
+ * To mark an attribute or method as ID type
+ */
+final class Identifier {
+    function __construct () {}
+}
